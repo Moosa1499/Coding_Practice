3808b13520f96e52c35988d5d0239ddab2bb3728
Moosa1499 <160492545+Moosa1499@users.noreply.github.com>
2024-04-30 04:46:40 +0500

    Add files via upload

---
+++ Project04/index.js
@@ -0,0 +1,5 @@
+"use strict";
+// "He who loves practice without theory is like the sailor who boards ship without a rudder and compass and never knows where he may cast." Leonardo da Vinc
+let famous_person = "Leonardo da Vinci";
+let Message = famous_person + ' Once said ,"He who loves practice without theory is like the sailor who boards ship without a rudder and compass and never knows where he may cast."';
+console.log(Message);
---
+++ Project04/index.ts
@@ -0,0 +1,7 @@
+// "He who loves practice without theory is like the sailor who boards ship without a rudder and compass and never knows where he may cast." Leonardo da Vinc
+
+
+
+let famous_person: string="Leonardo da Vinci";
+let Message:string=famous_person + ' Once said ,"He who loves practice without theory is like the sailor who boards ship without a rudder and compass and never knows where he may cast."';
+console.log(Message);
